42ac33f89e16b9bb065c38d34903dcb5f1cad5b6
Denis Shvetsov <shvetsovdandrsn@gmail.com>
2018-02-23 09:40:56 +0200

    Add project files.

---
+++ UmbracoSecondDemo/Controllers/ShoppingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoSecondDemo.Controllers
+{
+    public static class CartItemsKeeper
+    {
+        public static List<string> CartItems = new List<string>();
+    }
+
+    public class ShoppingController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult CartIndex()
+        {
+            var viewModel = CartItemsKeeper.CartItems;
+            return View("CartIndexPage", viewModel);
+        }
+
+        // GET: Shopping
+        [HttpGet]
+        [NotChildAction]
+        public bool AddToCart(string itemId)
+        {
+            CartItemsKeeper.CartItems.Add(itemId);
+            return true;
+        }
+    }
+}
